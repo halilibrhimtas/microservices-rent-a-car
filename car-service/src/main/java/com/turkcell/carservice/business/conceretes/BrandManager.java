50e90bc6f4286b307646a5f428e64881010ec088
halilibrhimtas <halilibrhim.tas@gmail.com>
2023-11-19 00:13:37 +0300

    car service completed

--- car-service/src/main/java/com/turkcell/carservice/business/conceretes/BrandManager.java
+++ car-service/src/main/java/com/turkcell/carservice/business/conceretes/BrandManager.java
@@ -1,20 +1,20 @@
 package com.turkcell.carservice.business.conceretes;
 
 import com.turkcell.carservice.business.abstracts.BrandService;
-import com.turkcell.carservice.domain.dtos.requests.brand.CreateBrandRequest;
-import com.turkcell.carservice.domain.dtos.requests.brand.UpdateBrandRequest;
-import com.turkcell.carservice.domain.dtos.responses.brand.CreateBrandResponse;
-import com.turkcell.carservice.domain.dtos.responses.brand.GetAllBrandResponse;
-import com.turkcell.carservice.domain.dtos.responses.brand.GetBrandResponse;
-import com.turkcell.carservice.domain.dtos.responses.brand.UpdateBrandResponse;
-import com.turkcell.carservice.domain.entities.Brand;
+import com.turkcell.carservice.business.rules.BrandsBusinessRules;
+import com.turkcell.carservice.business.dtos.requests.brand.CreateBrandRequest;
+import com.turkcell.carservice.business.dtos.requests.brand.UpdateBrandRequest;
+import com.turkcell.carservice.business.dtos.responses.brand.CreateBrandResponse;
+import com.turkcell.carservice.business.dtos.responses.brand.GetAllBrandResponse;
+import com.turkcell.carservice.business.dtos.responses.brand.GetBrandResponse;
+import com.turkcell.carservice.business.dtos.responses.brand.UpdateBrandResponse;
 import com.turkcell.carservice.repository.BrandRepository;
 import lombok.RequiredArgsConstructor;
+import com.turkcell.carservice.domain.entities.Brand;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +22,7 @@ public class BrandManager implements BrandService {
 
     private final BrandRepository brandRepository;
     private final ModelMapper modelMapper;
+    private final BrandsBusinessRules brandsBusinessRules;
 
 
     @Override
@@ -33,20 +34,25 @@ public class BrandManager implements BrandService {
 
     @Override
     public GetBrandResponse getById(String id) {
+        brandsBusinessRules.isExistBrand(id);
         Brand brand = brandRepository.findById(id).orElseThrow();
         return modelMapper.map(brand, GetBrandResponse.class);
     }
 
     @Override
     public CreateBrandResponse add(CreateBrandRequest request) {
-       // Brand brand = modelMapper.map(request, Brand.class);
+        // Brand brand = modelMapper.map(request, Brand.class);
+        brandsBusinessRules.brandMustHaveUniqueName(request.getName());
+
         Brand brand = Brand.builder().brandName(request.getName()).build();
         brandRepository.save(brand);
-        return modelMapper.map(brand, CreateBrandResponse.class);
+        return CreateBrandResponse.builder().id(brand.getId()).name(brand.getBrandName()).build();
     }
 
     @Override
     public UpdateBrandResponse update(String id, UpdateBrandRequest request) {
+        brandsBusinessRules.isExistBrand(id);
+        brandsBusinessRules.brandMustHaveUniqueName(request.getName());
         Brand brand = Brand.builder().id(id).brandName(request.getName()).build();
         brandRepository.save(brand);
         return modelMapper.map(brand, UpdateBrandResponse.class);
@@ -54,6 +60,7 @@ public class BrandManager implements BrandService {
 
     @Override
     public void delete(String id) {
+        brandsBusinessRules.isExistBrand(id);
         brandRepository.deleteById(id);
     }
 }
