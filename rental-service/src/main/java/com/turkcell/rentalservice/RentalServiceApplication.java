48f4c6a48c8f4a809ada4f4ce87de5dbaa04f9ab
halilibrhimtas <halilibrhim.tas@gmail.com>
2023-11-21 18:06:38 +0300

    updated

--- rental-service/src/main/java/com/turkcell/rentalservice/RentalServiceApplication.java
+++ rental-service/src/main/java/com/turkcell/rentalservice/RentalServiceApplication.java
@@ -2,7 +2,9 @@ package com.turkcell.rentalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 @SpringBootApplication
 public class RentalServiceApplication {
 
--- rental-service/src/test/java/com/turkcell/rentalservice/RentalServiceApplicationTests.java
+++ rental-service/src/test/java/com/turkcell/rentalservice/RentalServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.turkcell.rentalservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RentalServiceApplication.class)
 class RentalServiceApplicationTests {
 
 	@Test
